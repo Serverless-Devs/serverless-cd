6d33ddc5cd38fdf1968e124cb7a12c7412b94f32
wb-fc932227 <wb-fc932227@alibaba-inc.com>
2022-11-07 16:55:19 +0800

    修改 application, task接口 前缀（/flow）

--- src/console/ui/src/services/task.ts
+++ src/console/ui/src/services/task.ts
@@ -1,28 +1,28 @@
 import { request } from "ice";
 
 export const getTaskList = async (params) => {
-  const { data } = await request.get("/api/task/list", {
+  const { data } = await request.get("/api/flow/task/list", {
     params
   });
   return data
 };
 
 export const getTask = async (params) => {
-  const { data } = await request.get("/api/task/get", {
+  const { data } = await request.get("/api/flow/task/get", {
     params
   });
   return data
 };
 
 export const getTaskLog = async (params) => {
-  const { data } = await request.get("/api/task/log", {
+  const { data } = await request.get("/api/flow/task/log", {
     params
   });
   return data
 };
 
 export const removeTaskCommit = async (body) => {
-  return await request.post("/api/task/remove", body);
+  return await request.post("/api/flow/task/remove", body);
 };
 
 export const redeployTask = async (body) => {
