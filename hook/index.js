b39d753b59bfc4dedf6b8e56f5ea1b117ead76c1
heimanba <yuankun.yk@alibaba-inc.com>
2022-11-08 20:06:29 +0800

    feat: 🎸 update serverless-cd

--- hook/index.js
+++ hook/index.js
@@ -1,10 +1,12 @@
 const path = require("path");
+const core = require("./core");
+
 async function preInit(inputObj) {
   const logger = new inputObj.Logger("");
   if (process.env["CLI_VERSION"] < "2.1.7") {
     logger.log(
       'Please upgrade the CLI version to 2.1.7 or above, and you can execute the command "npm i @serverless-devs/s -g --registry=https://registry.npmmirror.com" to upgrade.',
-      "red"
+      "yellow"
     );
   }
 }
@@ -18,18 +20,14 @@ async function postInit(inputObj) {
     `);
   const logger = new inputObj.Logger("");
   
-  if(process.env["CLI_VERSION"] < "2.1.7") {
-    return logger.log(`\nPlease run exec " generate -t ${syaml} " init cloud resources \n`, "red");
-  }
-  
   try {
-    await inputObj.execCommand({
+    await core.execCommand({
       syaml,
       method: "generate",
     });
     console.log(`
     \n Cloud resource creation is complete
-    \n Execute the command " s deploy " and enjoy!
+    \n Run " s deploy "start Serverless CI/CD journey!
     `);
   } catch (ex) {
     logger.log(`\nGenerate resource error: ${ex.message}\n`, "red");
