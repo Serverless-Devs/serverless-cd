e8178dc394b5546121dde664bc9bfc6b450b5e5f
xsahxl <xsahxl@126.com>
2022-12-09 20:19:06 +0800

    test script

--- scripts/generate-env.js
+++ scripts/generate-env.js
@@ -1,4 +1,4 @@
-await $`cd('{{secrets.DOWNLOAD_CODE_DIR}}')`;
+await $`cd({{secrets.DOWNLOAD_CODE_DIR}})`;
 await $`pwd`;
 await $`echo SESSION_EXPIRATION={{secrets.SESSION_EXPIRATION}} >> .env`;
 await $`echo DOWNLOAD_CODE_DIR={{secrets.DOWNLOAD_CODE_DIR}} >> .env`;
