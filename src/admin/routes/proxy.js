6a2ec5d6569771fa18fe5c6e873fa740b4404461
huangzichun <313658356@qq.com>
2023-01-01 23:45:43 +0800

    feat: optimize the way of request returned

--- src/admin/routes/proxy.js
+++ src/admin/routes/proxy.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const httpx = require('httpx');
 const { lodash: _ } = require('@serverless-cd/core');
+const { NotFoundError } = require('../util/custom-errors');
+const { Result } = require("../util");
 
 /**
  * method: POST
@@ -17,30 +19,35 @@ router.post('/appCenter', async function (req, res, _next) {
   let baseUrl = '';
   const {type, urlPath } = req.body;
   if(type === 'github') {
-    if(!_.isEmpty(appCenterList)) {
-      return res.json(appCenterList);
-    }
-
-    baseUrl = 'https://api.github.com/';
-    const githubUrl = baseUrl + urlPath;
-    console.log('proxy url:', githubUrl);
-    const response = await httpx.request(githubUrl, {
-      method: 'GET',
-      timeout: 0,
-      headers: {
-       "User-Agent": 'serverless-cd' 
+    try {
+      if(!_.isEmpty(appCenterList)) {
+        return res.json(Result.ofSuccess({ appCenterList }));
       }
-    });
-    let result = await httpx.read(response, 'utf8');
-    const contentType = response.headers['content-type'] || '';
-    if (contentType.startsWith('application/json')) {
-      // TODO: Need to add additional messages when an error is thrown
-      result = JSON.parse(result);
+  
+      baseUrl = 'https://api.github.com/';
+      const githubUrl = baseUrl + urlPath;
+      console.log('proxy url:', githubUrl);
+      const response = await httpx.request(githubUrl, {
+        method: 'GET',
+        timeout: 0,
+        headers: {
+         "User-Agent": 'serverless-cd' 
+        }
+      });
+      let result = await httpx.read(response, 'utf8');
+      const contentType = response.headers['content-type'] || '';
+      if (contentType.startsWith('application/json')) {
+        // TODO: Need to add additional messages when an error is thrown
+        result = JSON.parse(result);
+      }
+      appCenterList = result;
+      return res.json(Result.ofSuccess({ appCenterList }));
+    } catch (error) {
+      return res.json(Result.ofError(error.message, NotFoundError))
     }
-    appCenterList = result;
-    return res.json(result);
+    
   }else {
-    return res.json({success: false});
+    return res.json(Result.ofError(error.message, NotFoundError));
   }
 });
 
