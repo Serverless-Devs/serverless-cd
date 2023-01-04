0f3b18c2bc26c69b3a7111a62d2791e7235953d1
xsahxl <xsahxl@126.com>
2023-01-04 14:44:24 +0800

    update:前端目录
    Signed-off-by: xsahxl <xsahxl@126.com>

--- src/admin/public/src/components/BasicInfoDetail.tsx
+++ src/admin/public/src/components/BasicInfoDetail.tsx
@@ -7,7 +7,7 @@ import ExternalLink from '@/components/ExternalLink';
 import CommitId from '@/components/CommitId';
 import { C_REPOSITORY } from '@/constants/repository';
 import Status from '@/components/DeployStatus';
-import RefreshButton from '@/pages/Details/components/RefreshButton';
+import RefreshButton from '@/pages/AppDetail/components/RefreshButton';
 interface Props {
   data: object;
   refreshCallback: Function;
@@ -30,82 +30,86 @@ const BasicInfoDetail = (props: Props) => {
     <BasicInfo
       title={'基本信息'}
       content={<RefreshButton refreshCallback={refreshCallback} />}
-      items={[
-        {
-          text: '仓库名称',
-          value: (
-            <div>
-              <span className='fz-12'>{owner}</span>
-              <span className='fz-14 f-w-500'> / {repo_name}</span>
-            </div>
-          ),
-        },
-        {
-          text: '描述',
-          value: description,
-        },
-        {
-          text: '创建时间',
-          value: created_time,
-        },
-        {
-          text: '部署状态',
-          value: (
-            <div className='flex-r' style={{ justifyContent: 'flex-start' }}>
-              <Status status={status as any} />
-              {
-                taskId && (<Link className='ml-8' to={`/application/${appId}/detail/${taskId}`} >查看</Link>)
-              }
-            </div>
-          )
-        },
-        {
-          text: '代码源',
-          value: (
-            <div className="align-center">
-              {C_REPOSITORY[provider as any]?.svg(16)}
-              <ExternalLink
-                className="color-link cursor-pointer ml-4"
-                url={repo_url}
-                label={repo_name}
-              />
-            </div>
-          ),
-        },
-        commit && {
-          text: 'Commit',
-          value: (
-            <div className="align-center">
-              {C_REPOSITORY[provider as any]?.svg(16)}
-              <CommitId
-                className="ml-4"
-                url={`https://${provider}.com/${owner}/${repo_name}/commit/${commit}`}
-                label={get(data, 'latest_task.commit') as string}
-                message={get(data, 'latest_task.message')}
-                icon={false}
-              />
-            </div>
-          ),
-        },
-        // {
-        //   text: '分支',
-        //   value: (
-        //     <div className="align-center">
-        //       {C_REPOSITORY['github']?.svg(16)}
-        //       <ExternalLink
-        //         className="color-link cursor-pointer ml-4"
-        //         url={`https://${provider}.com/${owner}/${repo_name}/tree/`}
-        //         // url={'https://github.com/heimanba/todolist-app-msfg/tree/master'}
-        //         label={'master'}
-        //       />
-        //     </div>
-        //   ),
-        // },
-        // on && {
-        //   text: '触发方式',
-        //   value: trigger[on],
-        // },
-      ].filter(Boolean) as any}
+      items={
+        [
+          {
+            text: '仓库名称',
+            value: (
+              <div>
+                <span className="fz-12">{owner}</span>
+                <span className="fz-14 f-w-500"> / {repo_name}</span>
+              </div>
+            ),
+          },
+          {
+            text: '描述',
+            value: description,
+          },
+          {
+            text: '创建时间',
+            value: created_time,
+          },
+          {
+            text: '部署状态',
+            value: (
+              <div className="flex-r" style={{ justifyContent: 'flex-start' }}>
+                <Status status={status as any} />
+                {taskId && (
+                  <Link className="ml-8" to={`/application/${appId}/detail/${taskId}`}>
+                    查看
+                  </Link>
+                )}
+              </div>
+            ),
+          },
+          {
+            text: '代码源',
+            value: (
+              <div className="align-center">
+                {C_REPOSITORY[provider as any]?.svg(16)}
+                <ExternalLink
+                  className="color-link cursor-pointer ml-4"
+                  url={repo_url}
+                  label={repo_name}
+                />
+              </div>
+            ),
+          },
+          commit && {
+            text: 'Commit',
+            value: (
+              <div className="align-center">
+                {C_REPOSITORY[provider as any]?.svg(16)}
+                <CommitId
+                  className="ml-4"
+                  url={`https://${provider}.com/${owner}/${repo_name}/commit/${commit}`}
+                  label={get(data, 'latest_task.commit') as string}
+                  message={get(data, 'latest_task.message')}
+                  icon={false}
+                />
+              </div>
+            ),
+          },
+          // {
+          //   text: '分支',
+          //   value: (
+          //     <div className="align-center">
+          //       {C_REPOSITORY['github']?.svg(16)}
+          //       <ExternalLink
+          //         className="color-link cursor-pointer ml-4"
+          //         url={`https://${provider}.com/${owner}/${repo_name}/tree/`}
+          //         // url={'https://github.com/heimanba/todolist-app-msfg/tree/master'}
+          //         label={'master'}
+          //       />
+          //     </div>
+          //   ),
+          // },
+          // on && {
+          //   text: '触发方式',
+          //   value: trigger[on],
+          // },
+        ].filter(Boolean) as any
+      }
       sizePerRow={2}
     />
   );
