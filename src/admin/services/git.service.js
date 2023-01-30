960a95070b6f29daa1413b9def5dcb1a03ea3a13
heimanba <371510756@qq.com>
2023-01-30 10:42:52 +0800

    feat: 🎸 通过模版创建应用
    ✅ Closes: https://github.com/serverless-cd/serverless-cd/issues/22

---
+++ src/admin/services/git.service.js
@@ -0,0 +1,27 @@
+module.exports = {
+  downloadCode() {
+    // case1: 使用Serverless-dev下载&解压到指定的目录
+    // case2: 使用git源下载并解压
+  },
+  createRepo() {
+    /**
+     * 1. createRepo
+     *  默认是private
+     *  默认分支为master
+     * 2. 设置分支默认保护
+     * 3. 添加webhook
+     */
+  },
+  commitAndPush() {
+    /**
+     * git commit
+     * git push
+     */
+  },
+  deleteWebhook() {
+
+  },
+  deleteRepo() {
+
+  }
+};
