5a44a54ee792fecbfb384528d10c21bba7860df4
wss-git <wssgryx@163.com>
2023-02-20 17:51:57 +0800

    fix: 邀请用户异常
    Signed-off-by: wss-git <wssgryx@163.com>

--- src/master/model/index.js
+++ src/master/model/index.js
@@ -21,39 +21,15 @@ async function getUserById(id) {
 }
 
 /**
- * 根据 orgId 获取团队信息
- * @param {*} id 
- * @returns 
- */
-async function getOrgById(id) {
-  const result = await orgPrisma.findUnique({ where: { id } });
-  if (result && result.secrets) {
-    result.secrets = JSON.parse(result.secrets);
-  }
-  return result;
-}
-
-/**
  * 根据团队Id拿到拥有者用户数据
  */
 async function getOrganizationOwnerIdByOrgId(orgId) {
-  let ownerUserId = '';
-  // 当前用户在此团队的数据
-  const orgData = await getOrgById(orgId);
-  const { role, name = '' } = orgData || {};
-  if (role === ROLE.OWNER) {
-    ownerUserId = orgData.user_id;
-  } else {
-    const ownerOrgData = await orgPrisma.findFirst({ where: { name, role: ROLE.OWNER } });
-    ownerUserId = ownerOrgData.user_id;
-  }
-
-  // TODO
-  // 此团队拥有者的数据：一个团队只能拥有一个 owner
-  const userConfig = await getUserById(ownerUserId);
+  const [, name] = _.split(orgId, ':');
+  const ownerOrgData = await orgPrisma.findFirst({ where: { name, role: ROLE.OWNER } });
+  const userConfig = await getUserById(ownerOrgData.user_id);
   return {
     ...userConfig,
-    secrets: orgData.secrets,
+    secrets: ownerOrgData.secrets,
   };
 }
 
