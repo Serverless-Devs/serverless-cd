9ebaa7eabe65aabab59a8ec5c7df16a4deb1c05e
wb-fc932227 <wb-fc932227@alibaba-inc.com>
2023-03-06 14:18:23 +0800

    新增模版列表

---
+++ src/admin/public/src/pages/Create/components/template/index.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState, forwardRef, useImperativeHandle } from 'react';
+import TemplateContent from './TemplateContent';
+import TemplateList from './TemplateList';
+import { getParam } from '@/utils';
+import { noop } from 'lodash';
+
+
+const CreateTemplate = ({ field, forceUpdate = noop }, ref) => {
+  const [template, setTemplate] = useState();
+
+  useImperativeHandle(ref, () => ({
+    template
+  }))
+
+  useEffect(() => {
+    const template: any = getParam('template');
+    setTemplate(template);
+  }, [])
+
+  return (
+    <>
+      {
+        template ? (
+          <TemplateContent field={field} template={template} forceUpdate={forceUpdate} />
+        ) : (
+          <TemplateList forceUpdate={forceUpdate} />
+        )
+      }
+    </>
+  )
+}
+
+
+export default forwardRef(CreateTemplate);
--- src/admin/public/src/pages/Create/index.tsx
+++ src/admin/public/src/pages/Create/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Field } from '@alicloud/console-components';
 import PageLayout from '@/layouts/PageLayout';
 import CreateType, { CREATE_TYPE } from './components/CreateType';
 import Github from './components/github/index';
+import CreateTemplate from './components/template/index';
 import Submit from './components/Submit';
+import { setSearchParams, getParam } from '@/utils/index';
 import './index.less';
 
 const Create = ({
@@ -30,14 +32,21 @@ const Create = ({
 
 export const CreateAppLication = ({ orgName }) => {
   const field = Field.useField();
-  const { init, getValue } = field;
+  const [createType, setCreateType] = useState(CREATE_TYPE.Template);
+  const [pageKey, forceUpdate] = useState(0);
+  const templateRef: any = useRef(null);
+  const template = getParam('template');
+  const onChangeCreateType = (type) => {
+    setSearchParams({ template: '' });
+    setCreateType(type);
+  };
 
   return (
     <div className="appliaction-create-container">
-      <CreateType {...init('createType', { initValue: CREATE_TYPE.Repository })} />
-      {getValue('createType') === CREATE_TYPE.Repository && <Github field={field} />}
-      {getValue('createType') === CREATE_TYPE.Template && <h1>敬请期待...</h1>}
-      {getValue('createType') === CREATE_TYPE.Repository && (
+      {!template && <CreateType value={createType} onChange={onChangeCreateType} />}
+      {createType === CREATE_TYPE.Template && <CreateTemplate key={pageKey} forceUpdate={() => forceUpdate(pageKey + 1)} field={field} ref={templateRef} />}
+      {createType === CREATE_TYPE.Repository && <Github field={field} />}
+      {createType === CREATE_TYPE.Repository && (
         <Submit field={field} orgName={orgName} />
       )}
     </div>
