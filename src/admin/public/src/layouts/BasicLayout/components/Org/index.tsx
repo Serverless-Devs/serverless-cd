7f035b331e6c1d04f53b58b07e8a5ddc69359914
xsahxl <xsahxl@126.com>
2023-03-27 15:24:53 +0800

    feat: 左上角组织
    Signed-off-by: xsahxl <xsahxl@126.com>

--- src/admin/public/src/layouts/BasicLayout/components/Org/index.tsx
+++ src/admin/public/src/layouts/BasicLayout/components/Org/index.tsx
@@ -1,16 +1,17 @@
 import React, { FC, useEffect } from 'react';
-import { Dropdown, Menu, Icon } from '@alicloud/console-components';
+import { Select } from '@alicloud/console-components';
 import { history, useRequest } from 'ice';
 import style from 'styled-components';
 import { ORG_LOGO } from '@/constants/public';
-import { localStorageSet, stopPropagation } from '@/utils';
+import { localStorageSet } from '@/utils';
 import { listOrgs } from '@/services/user';
-import { get, map } from 'lodash';
+import { get, map, find } from 'lodash';
+import './index.less';
 
-const StyledMenu = style.div`
-.next-menu{
-  width: 180px;
-}
+const StyledWrapper = style.div`
+  .next-input {
+    border: unset;
+  }
 `;
 
 type Props = {
@@ -25,66 +26,38 @@ const Org: FC<Props> = (props) => {
     orgName && orgRequest.request();
   }, [orgName]);
 
-  const menu = () => {
-    const onItemClick = (key: string) => {
-      history?.push(`/${orgName}${key}`);
-    };
-
-    const handleChangeOrg = async (item) => {
-      localStorageSet(item.user_id, item.name);
-      history?.push('/');
-    };
-    const orgRender = (
-      <Dropdown
-        trigger={
-          <div onClick={stopPropagation} className="flex-r">
-            <span>团队切换</span>
-            <Icon type="arrow-right" size="xs" style={{ color: '#888' }} />
-          </div>
-        }
-        offset={[168, -38]}
-      >
-        <Menu>
-          {map(get(orgRequest, 'data.result'), (item) => {
-            return (
-              <Menu.Item>
-                <div className="flex-r" onClick={() => handleChangeOrg(item)}>
-                  <span className="ellipsis">{item.name}</span>
-                  {orgName === item.name && (
-                    <Icon type="select" size="xs" style={{ color: '#0070cc' }} />
-                  )}
-                </div>
-              </Menu.Item>
-            );
-          })}
-        </Menu>
-      </Dropdown>
-    );
-
-    return (
-      <StyledMenu>
-        <Menu onItemClick={onItemClick}>
-          <Menu.Item className="border-bottom">{orgRender}</Menu.Item>
-          <Menu.Item key="/settings">团队设置</Menu.Item>
-        </Menu>
-      </StyledMenu>
-    );
+  const handleChangeOrg = async (value) => {
+    const obj = find(dataSource, (item) => item.value === value);
+    localStorageSet(obj.user_id, value);
+    history?.push('/');
   };
-  return (
-    <Dropdown
-      trigger={
-        <div className="layout-center cursor-pointer">
+
+  const dataSource = map(get(orgRequest, 'data.result'), (item) => {
+    return {
+      ...item,
+      label: (
+        <div className="align-center">
           <img
-            src={ORG_LOGO}
+            src={item.logo || ORG_LOGO}
             style={{ width: 24, height: 24, margin: '0 8', borderRadius: '50%' }}
           />
-          <span className="ml-4 fz-18 text-bold ellipsis">{orgName}</span>
+          <span className="ml-8 ellipsis">{item.alias || item.name}</span>
         </div>
-      }
-      offset={[0, 0]}
-    >
-      {menu()}
-    </Dropdown>
+      ),
+      value: item.name,
+    };
+  });
+
+  return (
+    <StyledWrapper>
+      <Select
+        value={orgName}
+        style={{ width: 150 }}
+        dataSource={dataSource}
+        onChange={handleChangeOrg}
+        popupClassName="org-select__container"
+      />
+    </StyledWrapper>
   );
 };
 
