332464655c91a0ac4983ebacb917fa723deed61b
mhp01056531 <884103375@qq.com>
2023-04-10 11:58:55 +0800

    组织图片调整

--- src/admin/public/src/layouts/BasicLayout/components/Org/index.tsx
+++ src/admin/public/src/layouts/BasicLayout/components/Org/index.tsx
@@ -34,11 +34,13 @@ const Org: FC<Props> = (props) => {
         ...item,
         label: (
           <div className="align-center">
-            <img
-              src={item.logo || ORG_LOGO}
-              style={{ width: 20, height: 20, margin: '0 8', overflow: 'hidden' }}
-            />
-            <span className="ml-8 ellipsis" style={{ width: 86 }}>{item.alias || item.name}</span>
+            <div className='org-image'>
+              <img
+                src={item.logo || ORG_LOGO}
+                style={{ width: 20, height: 20, margin: '0 8', overflow: 'hidden' }}
+              />
+            </div>
+            <span className="ml-8 ellipsis">{item.alias || item.name}</span>
           </div>
         ),
         value: item.name,
