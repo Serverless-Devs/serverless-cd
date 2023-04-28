0ca380d6a41afc88841b1aaa2bcb61a40602210f
wss <wssgryx@163.com>
2023-04-28 15:58:01 +0800

    feat: 新增注释 全局密钥获取方式
    Signed-off-by: wss <wssgryx@163.com>

--- src/admin/util/client.js
+++ src/admin/util/client.js
@@ -2,6 +2,10 @@ const _ = require('lodash');
 const Fc = require('@serverless-cd/srm-aliyun-fc2');
 const { CREDENTIALS } = require('@serverless-cd/config');
 
+// // 获取密钥配置
+// const accountId = envs.ACCOUNT_ID || envs.FC_ACCOUNT_ID;
+// const accessKeyId = envs.ACCESS_KEY_ID || envs.ALIBABA_CLOUD_ACCESS_KEY_ID;
+// const accessKeySecret = envs.ACCESS_KEY_SECRET || envs.ALIBABA_CLOUD_ACCESS_KEY_SECRET;
 const { accessKeyId, accessKeySecret, accountId, securityToken } = CREDENTIALS;
 
 module.exports = class Client {
@@ -12,7 +16,7 @@ module.exports = class Client {
       securityToken,
       region,
       timeout: 60 * 1000,
-      internal: !!process.env.FC_QUALIFIER, // TODO: 区分环境
+      // internal: !!process.env.FC_QUALIFIER, // TODO: 区分环境
     });
   }
 
